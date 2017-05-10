716fcf9e7351a31a76e0288308fb6a759f2bff4a
hongyimin <hongyimin@DCKJ-IOS-1s-Mac-mini.local>
2017-05-10 14:12:28 +0800

    Initial Commit

---
+++ ImageBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageBrowser
+//
+//  Created by hongyimin on 2017/5/10.
+//  Copyright © 2017年 hongyimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageBrowser
+//
+//  Created by hongyimin on 2017/5/10.
+//  Copyright © 2017年 hongyimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
